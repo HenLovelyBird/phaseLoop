630610ab76c4d443258f7e46342569dc6170630d
henlovelybird <voxylophone@gmail.com>
2020-10-01 16:46:45 +0200

    adding player

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -6,9 +6,9 @@ fetch('itsgonnarainClip.wav')
     .then(audioBuffer => {
         let sourceNode = audioContext.createBufferSource();
         sourceNode.buffer = audioBuffer;
-        sourceNode.loop = false;
+        sourceNode.loop = true;
         sourceNode.loopStart = 5.00;
-        sourceNode.loopEnd = 9.00;
+        sourceNode.loopEnd = 12.00;
         sourceNode.connect(audioContext.destination);
         sourceNode.start(0, 5.00);
     })
